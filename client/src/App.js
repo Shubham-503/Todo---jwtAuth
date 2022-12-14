0a8ead11373ecbd57a673b0aa6a445459e741ca5
Shubham-503 <kr.shubham025@gmail.com>
2022-12-14 11:13:32 +0530

    Added Cancel Search button

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ function App() {
   const [searchInput, setSearchInput] = useState("")
   const [isAuth, setIsAuth] = useState(false)
   const [id, setId] = useState(false)
+  const [isSearchActive, setIsSearchInput] = useState(false)
   const history = useHistory()
   const cookies = new Cookies();
 
@@ -63,6 +64,7 @@ function App() {
 
   const handleSearch = async (e,query) => {
     e.preventDefault();
+    setIsSearchInput(true)
     console.log('search called');
     try {
       const res = await axios.get(`/search?q=${query}`)
@@ -71,6 +73,23 @@ function App() {
     } catch (error) {
       console.log(error.message);
     }
+  }
+
+  const searchClose = (e) => {
+    e.preventDefault()
+    setIsSearchInput(false)
+    getTodos()
+    setSearchInput("")
+  }
+
+  const handleSort = async (e,order)=>{
+    try {
+      const res = await axios.get(`/gettodosbyorder?q=${order}`)
+      setTodos(res.data.todos)
+    } catch (error) {
+      console.log(error.message) 
+    }
+   
   }
 
   return (
@@ -83,6 +102,20 @@ function App() {
           {isAuth ? (
             <>
               <TodoForm createTodo={createTodo} />
+               {/* Search option */}
+               <div className={` ${isSearchActive ? "flex" : "hidden"}  flex-row justify-between items-center bg-white p-6 border rounded-md antialiased   text-slate-700`}>
+                <div>
+                  <h1 className="text-3xl font-medium">Search Result</h1>
+                </div>
+                <div className="inline-flex space-x-2 items-center ml-12">
+                  <button href="#"
+                    className="p-2 border border-red-200 rounded-md inline-flex space-x-1 items-center bg-red-600 hover:bg-red-500"
+                    onClick={(e) => { searchClose(e) }}
+                  >
+                    <i className="fa-solid fa-times text-white" ></i>
+                  </button>
+                </div>
+              </div>
               <TodoList todos={todos} getTodos={getTodos} />
               <div className="flex justify-between items-center absolute top-0 right-4">
                 <div id="task-input" class="flex justify-between items-center border-b border-slate-200 py-3 px-2 border-l-4  border-l-transparent w-full">
@@ -94,6 +127,14 @@ function App() {
                       </button>
                     </div>
                   </form>
+                </div>
+                <div className="order-btns">
+                  <button className="p-2 border border-slate-200 rounded-md inline-flex space-x-1 items-center text-indigo-200 hover:text-white bg-indigo-600 hover:bg-indigo-500" onClick={(e) => { handleSort(e, "asc") }} >
+                    <i className="fa-solid fa-sort-up"></i>
+                  </button>
+                  <button href="#" className="p-2 border border-slate-200 rounded-md inline-flex space-x-1 items-center text-indigo-200 hover:text-white bg-indigo-600 hover:bg-indigo-500" onClick={(e) => { handleSort(e, "dsc") }}>
+                    <i className="fa-solid fa-sort-down" ></i>
+                  </button>
                 </div>
                 <button className="mt-5   border p-2 bg-gradient-to-r from-gray-800 bg-gray-500 text-white rounded-[4px] hover:bg-slate-400 scale-105 duration-300" type="submit" onClick={(e) => { handleSignOut(e) }} color="white" >Sign Out</button>
               </div>
